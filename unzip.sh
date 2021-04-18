ce48025358c8f58ecbadc4448841b8f41a6e88a6
cdezz <ciaran.desmond@gmail.com>
2021-04-18 12:02:17 +0100

    minor changes

--- unzip.sh
+++ unzip.sh
@@ -2,5 +2,18 @@ inotifywait -m /home/ciaran/VScode/healthmailSocket/ -e create -e moved_to |
     while read path action file; do
         if [[ "$file" =~ .*zip$ ]]; then
             echo "$file"
-        fi
-    done
+            unzip "$file" -d "scriptsToPrint"
+            for file in ./scriptsToPrint/*; do
+               if [[ "$file" =~ .*html$ ]]; then
+                echo "'$file' is html"
+                # python -m weasyprint "$file" "$file.pdf"
+                #lp -d mcf... $file.pdf
+                rm "$file" # "$file.pdf"
+               else
+                echo "file is NOT html"
+                # python -m weasyprint "$file"
+                rm "$file"
+               fi;
+            done;
+        fi;
+    done;
