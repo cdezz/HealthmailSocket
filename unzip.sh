51ad263d5eff159c9177ce3d874f47f5948a0116
cdezz <ciaran.desmond@gmail.com>
2021-05-21 17:39:29 +0100

    fixed indentation error

--- unzip.sh
+++ unzip.sh
@@ -8,8 +8,9 @@ inotifywait -m /home/pi/HealthmailSocket/ -e create -e moved_to |
                if [[ "$file" =~ .*html$ ]]; then
                 echo "'$file' is html"
                 python3 -m weasyprint "$file" "$file.pdf"
-               lp -d  Brother_MFC-L2720DW_series_via_VNC_from_MPS01 $file.pdf
-                rm "$file"  "$file.pdf"
+                lp -d  Brother_MFC-L2720DW_series_via_VNC_from_MPS01 $file.pdf
+                rm "$file"  
+		rm "$file.pdf"
                else
                 echo "file is NOT html"
                 lp -d  Brother_MFC-L2720DW_series_via_VNC_from_MPS01 $file
